558049af7a695ab5e8476e7af074b45eac66cbe9
randynugraha16 <randynugraha11@gmail.com>
2021-08-16 20:26:18 +0700

    deploy heroku

--- seed.js
+++ seed.js
@@ -1034,7 +1034,7 @@ var data = [
         memberId: mongoose.Types.ObjectId("5e96cbe292b97300fc903333"),
         bankId: mongoose.Types.ObjectId("5e96cbe292b97300fc903323"),
         payments: {
-          proofPayment: "images/bukti.jpeg",
+          proofPayment: "images/buktibayar.jpeg",
           bankFrom: "BCA",
           status: "Proses",
           accountHolder: "ang",
@@ -1089,12 +1089,12 @@ var data = [
       {
         _id: mongoose.Types.ObjectId("5e96cbe292b97300fc903345"),
         username: "admin",
-        password: "rahasia",
+        password: "Singapur99",
       },
       {
         _id: mongoose.Types.ObjectId("5e96cbe292b97300fc903346"),
         username: "superadmin",
-        password: "rahasia",
+        password: "Singapur99",
         role: "admin",
       },
     ],
